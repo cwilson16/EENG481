8049d550e7ff16aa50b334cfd71204f22ddd882e
Wilson <cwilson16@zagmail.gonzaga.edu>
2022-02-15 09:11:07 -0800

    class for ping client stubbed out

---
+++ src/com/company/UDPPingClient.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.net.*;
+import java.io.*;
+
+public class UDPPingClient {
+
+    public static void main(String[] args) {
+        /*
+        DatagramSocket serverPing; 
+        DatagramPacket query, response;
+        int serverPort = 12000;
+        double rand;
+        try {
+            serverPing = new DatagramSocket(serverPort);
+            while (true) {
+                query = new DatagramPacket(new byte[128], 128);
+                serverPing.receive(query);
+                rand = 10*Math.random();
+                if (rand<3) {
+                    continue;
+                }
+                InetAddress clientIP = query.getAddress();
+                int clientPort = query.getPort();
+                System.out.println(new String(query.getData()));
+                response = new DatagramPacket(query.getData(), 0, query.getData().length, clientIP, clientPort);
+                serverPing.send(response);
+            }
+        } catch(IOException e) {
+            System.out.println("Could not establish server!");
+        }
+
+    }*/
+
+}
