dbab7e6e9f5e2e67a1f769ae2d1a258db15048b3
Chance Wilson <cwilson16@zagmail.gonzaga.edu>
2022-02-22 11:24:47 -0800

    getting there

--- src/com/company/UDPPingClient.java
+++ src/com/company/UDPPingClient.java
@@ -2,23 +2,47 @@ package com.company;
 
 import java.net.*;
 import java.io.*;
+import java.util.Date;
+import java.util.Scanner;
 
 public class UDPPingClient {
 
-    public static void main(String[] args) {/*
-
-        DatagramSocket clientPing;
+    public static void main(String[] args) {
+        DatagramSocket clientSocket;
         DatagramPacket query, response;
-        int serverPort = 12000;
-        double rand;
-        try {
-            System.out.println("yolo");
+        Scanner input = new Scanner(System.in); // couldn't import textIO library...
+        //String message = input.nextLine(); // so i'm trying to do it with scanner.
+        //System.out.println(message.toUpperCase(Locale.ROOT));
 
-
-        } catch(IOException e) {
-            System.out.println("Could not establish server!");
+        for(int i = 0; i < 10; i++) {
+            try {
+                Date now = new Date();
+                String message = "Ping " + i + " " + now.toString();
+                int serverPort = 12000;
+                InetAddress serverIP = InetAddress.getByName("localhost");
+                clientSocket = new DatagramSocket();
+                query = new DatagramPacket (message.getBytes(), message.getBytes().length, serverIP, serverPort);
+                float timeSent = System.currentTimeMillis();
+                clientSocket.send(query);
+                response = new DatagramPacket(new byte[1024], 1024);
+                clientSocket.receive(response);
+                float timeReceived = System.currentTimeMillis();
+                float roundTripTime = timeSent - timeReceived;
+                String testResponse = new String(response.getData(), response.getOffset(), response.getLength());
+                System.out.println("Response: " + testResponse);
+                System.out.println("Round Trip time was: " + roundTripTime + " milliseconds.");
+                //System.out.println(new String(response.getData()));
+                clientSocket.close();
+            }
+            catch (IOException e) {
+                System.out.println("Failed to communicate with server.");
+            }
         }
-*/
+
+
+
+
+
     }
 
 }
